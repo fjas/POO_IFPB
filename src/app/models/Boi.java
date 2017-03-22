2720a64f09eaa1f5899e3c07f102b2dff6be5a2f
Fagner Fjas <fagnerfjas@gmail.com>
2017-03-22 16:31:18 -0300

    JPA e hibernate

--- src/app/models/Boi.java
+++ src/app/models/Boi.java
@@ -1,6 +1,15 @@
 package app.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Boi {
+	
+	@Id
+	@GeneratedValue
+	public long id;
 	
 	public enum Categorias{
 		ASPIRANTE, AMADOR, INTERMEDIARIO, PROFISSIONAL
